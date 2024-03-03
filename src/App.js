e66d0b1be8f3fd5d10e1e473168fdfa630980b26
Jitendra mali <jitendramali.20.cs@iite.indusuni.ac.in>
2024-03-03 16:35:27 +0530

    Set up project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./Main";
+import history from "./Utils/history";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={Main}></Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
