28c57213cc9e4ea8eb1d9f6e0b44b29a8c9e05f2
Jitendra mali <jitendramali.20.cs@iite.indusuni.ac.in>
2024-03-08 14:56:33 +0530

    Solved the reducer issues

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./Reducers/rootReducer.js";
 
-const store = configureStore({ rootReducer });
+const store = configureStore({ reducer: rootReducer });
 
 export default store;
